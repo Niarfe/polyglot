4bb0522dc5e5afd8db1097cb79e409acad7699b3
EfrainOlivaresEv <eolivares@evidation.com>
2021-04-14 00:57:11 -0700

    add the ger case

--- verb.py
+++ verb.py
@@ -1,6 +1,7 @@
 import re
 import sys
-
+from pryzm import Pryzm
+red = Pryzm(echo=False).red
 
 class VerbFactory:
     def __init__(self):
@@ -9,13 +10,16 @@ class VerbFactory:
     def get_verb_obj(self, verb):
         if verb.endswith('cer'):
             return VerbCer(verb)
-        if verb.endswith('er'):
+        elif verb.endswith('ger'):
+            return VerbGer(verb)
+        elif verb.endswith('er'):
             return VerbEr(verb)
         else:
             None
 
 class VerbEr:
     def __init__(self, verb):
+        self.verb_id = 'er'
         self.verb = verb
         self.root = self.get_root()
 
@@ -46,20 +50,32 @@ class VerbEr:
     def elles(self):
         return self.root + 'ent'
 
+
 class VerbCer(VerbEr):
     def __init__(self, verb):
         super().__init__(verb) 
+        self.verb_id = 'cer'
 
     def nous(self):
         # Er verbs ending in cer use cedilla to avoid hard C sound before an O
         return self.root[:-1] + 'çons'
 
+class VerbGer(VerbEr):
+    def __init__(self, verb):
+        super().__init__(verb) 
+        self.verb_id = 'ger'
 
+    def nous(self):
+        # Er verbs ending in ger insert an e to avoid 'gons', to get 'geons' 
+        return self.root + 'eons'
 
-def test_conjugate_parler():
+# =========================== TESTS =======================================
+
+def test_verbs_ending_in_er():
     vFact = VerbFactory()
     verb = vFact.get_verb_obj('parler')
 
+    assert verb.verb_id == 'er'
     assert verb.get_root() == 'parl'
     assert verb.je() == 'parle'
     assert verb.tu() == 'parles'
@@ -70,39 +86,11 @@ def test_conjugate_parler():
     assert verb.ils() == 'parlent'
     assert verb.elles() == 'parlent'
 
-
-def test_conjugate_chanter():
-    vFact = VerbFactory()
-    verb = vFact.get_verb_obj('chanter')
-
-    assert verb.get_root() == 'chant'
-    assert verb.je() == 'chante'
-    assert verb.tu() == 'chantes'
-    assert verb.il() == 'chante'
-    assert verb.elle() == 'chante'
-    assert verb.nous() == 'chantons'
-    assert verb.vous() == 'chantez'
-    assert verb.ils() == 'chantent'
-    assert verb.elles() == 'chantent'
-
-def test_conjugate_annoncer():
-    vFact = VerbFactory()
-    verb = vFact.get_verb_obj('annoncer')
-
-    assert verb.get_root() == 'annonc'
-    assert verb.je() == 'annonce'
-    assert verb.tu() == 'annonces'
-    assert verb.il() == 'annonce'
-    assert verb.elle() == 'annonce'
-    assert verb.nous() == 'annonçons'
-    assert verb.vous() == 'annoncez'
-    assert verb.ils() == 'annoncent'
-    assert verb.elles() == 'annoncent'
-
-def test_conjugate_défoncer():
+def test_verbs_ending_in_cer():
     vFact = VerbFactory()
     verb = vFact.get_verb_obj('défoncer')
 
+    assert verb.verb_id == 'cer'
     assert verb.get_root() == 'défonc'
     assert verb.je() == 'défonce'
     assert verb.tu() == 'défonces'
@@ -112,6 +100,21 @@ def test_conjugate_défoncer():
     assert verb.vous() == 'défoncez'
     assert verb.ils() == 'défoncent'
     assert verb.elles() == 'défoncent'
+
+def test_verbs_ending_in_ger():
+    vFact = VerbFactory()
+    verb = vFact.get_verb_obj('voyager')
+
+    assert verb.verb_id == 'ger'
+    assert verb.get_root() == 'voyag'
+    assert verb.je() == 'voyage'
+    assert verb.tu() == 'voyages'
+    assert verb.il() == 'voyage'
+    assert verb.elle() == 'voyage'
+    assert verb.nous() == 'voyageons'
+    assert verb.vous() == 'voyagez'
+    assert verb.ils() == 'voyagent'
+    assert verb.elles() == 'voyagent'
 
 if __name__ == '__main__':
     import sys
