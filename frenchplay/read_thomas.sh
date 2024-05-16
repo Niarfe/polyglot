dff7daae85d34e446d46199a201d231e881a8b9e
Niarfe <efrain.olivares@gmail.com>
2024-05-15 20:48:06 -0700

    Init frenchplay

---
+++ frenchplay/read_thomas.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+for word
+do
+    echo $word 
+    Say -v 'Thomas (Enhanced)' --rate 110 $word
+done
+
---
+++ frenchplay/showntell.sh
@@ -0,0 +1,7 @@
+#!/usr/local/bin/bash
+for word
+do
+    echo $word 
+    Say -v Audrey $word
+done
+
